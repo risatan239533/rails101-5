830f8893381b3877f4b481f1a3e85b5df1b7bc94
RISA <risa@RISAdeMacBook-Pro.local>
2017-04-05 06:54:34 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -50,6 +50,8 @@ end
       if current_user != @group.user
         redirect_to root_path, alert: "You have no permission."
       end
+    end
+  
 
   def group_params
        params.require(:group).permit(:title, :description)
