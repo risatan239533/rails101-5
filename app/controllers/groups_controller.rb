8dc40a382412dc0aa09d519c98591a1f40554fa4
RISA <risa@RISAdeMacBook-Pro.local>
2017-04-05 21:37:20 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory, :join , :quit]
+
 
   def index
     @groups = Group.all
@@ -42,7 +43,31 @@ end
     @group.destroy
     redirect_to groups_path, alert: "删除成功"
   end
+  def join
+     @group = Group.find(params[:id])
 
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本评论成功！"
+      else
+        flash[:warning] = "你已经是本评论成员了！"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出本评论！"
+      else
+        flash[:warning] = "你不是本评论成员，怎么退出 XD"
+      end
+
+      redirect_to group_path(@group)
+    end
 
   private
   def find_group_and_check_permission
